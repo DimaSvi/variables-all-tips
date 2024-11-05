f6d7b19865a4fde0e850f1dcbb72bed5af35f29b
DimaSvi <dmytro.v.svitlyk@ukd.edu.ua>
2024-11-05 19:54:13 +0200

    Add files via upload

---
+++ variables tips.js
@@ -0,0 +1,6 @@
+const computername = "LapTop";
+const computeryear = "2020";
+let compuetprice = 30000;
+let avaible = true;
+let buyername = null;
+console.log('Назва: ${computername}, рік ${computeryear}, Ціна ${computerprice} гривень, Доступ: ${avaible}, покупець: ${buyername}')
